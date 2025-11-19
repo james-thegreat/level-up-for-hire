1703b9e2428b978a5ac601440027ab4aeb2982a2
James Dunlop <jamezdunlop@gmail.com>
2025-11-19 15:45:45 +1300

    update LeetCode

---
+++ LeetCode/problems/1-two-sum/solution.py
@@ -0,0 +1,10 @@
+#function to find two numbers that add up to a specific target
+def two_sum(nums, target):
+    
+    solution  = nums
+
+    for i in len.nums:
+        solution = target - nums
+        if target == soution 
+            print(solution)
+        
