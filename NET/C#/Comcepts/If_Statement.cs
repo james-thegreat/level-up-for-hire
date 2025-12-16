9bb79f9c474f645227af20fe6be7960cfebd90fe
James Dunlop <jamezdunlop@gmail.com>
2025-12-16 15:54:52 +1300

    practicing user concepts: user input, Random Number, if staments, while loop.

---
+++ NET/C#/Comcepts/If_Statement.cs
@@ -0,0 +1,12 @@
+using System;
+
+class ifStatement {
+
+    void randNum() {
+        Random random = new Random();
+        return random;
+    }
+    static void main() {
+        Console.WriteLine(randNum);
+    }
+}
